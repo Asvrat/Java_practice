ea83c0caf550e64b91948654e91d977606e8f056
Asvrat <manulandx@gmail.com>
2023-03-20 21:38:01 +0300

    com_2

---
+++ untitled/src/DataType.java
@@ -0,0 +1,8 @@
+public class DataType {
+    public static void main(String[] args) {
+        double t = Math.PI/2;
+        double sint = Math.sin(t);
+        double cost = Math.cos(t);
+        System.out.println(sint + cost);
+    }
+}
--- untitled/src/RandomNumber.java
+++ untitled/src/RandomNumber.java
@@ -1,11 +1,20 @@
-/*
-public class RandomNumber {
+/*public class RandomNumber {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int abs = Math.abs(a-b);
-        int res = (int) (Math.random()*abs + Math.abs());
+        int a = 3;
+        int b = 18;
+        double R = Math.random();
+        int res = a + ((int) (R * (b - a)));
         System.out.println(res);
     }
+}*/
+public class RandomNumber {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 7;
+        double R = Math.random();
+        int x = a + ((int) (R * (b - a)));
+        R = Math.random();
+        int y = a + ((int) (R * (b - a)));
+        System.out.println(x+y);
+    }
 }
-*/
